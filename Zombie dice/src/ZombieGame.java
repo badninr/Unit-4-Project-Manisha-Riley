944314e97f8e97e56e855c26b640bc55dc3e8cf0
badninr <54951047+badninr@users.noreply.github.com>
2019-12-10 09:43:10 -0800

    Add files via upload
    Initial zombie dice game

---
+++ Zombie dice/src/ZombieGame.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class ZombieGame {
+    private int brainCount;
+    private int shotgunCount;
+    private int dieColor;
+    private int roundNum;
+    private int currentPlayer;
+
+    private ArrayList<Integer> colors;
+
+    public int selectColor() {
+        for (int i=1; i<=13; i++)
+            colors.add(i);
+
+        int colorSelection = (int) Math.random()*13 + 1;
+
+        colors.remove(colorSelection);
+
+    }
+}
+
